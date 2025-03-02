74b9298793a22cb8a3ef10b2bcb8ea926fa4c3a0
meepstertron <meepstertron@gmail.com>
2025-03-02 00:19:16 -0600

    Add motivational print statement for resource maximization

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import subprocess
+import psutil
 
 def main():
     print("Hello from resource-maxxing!")
+    print("its time to rizz those gpus and rev those cpus to the max!")
+    
 
 
 if __name__ == "__main__":
